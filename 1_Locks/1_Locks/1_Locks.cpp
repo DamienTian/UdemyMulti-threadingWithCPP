34befac43d740698038b2947589a7bcac2b39e23
Hao Tian <tian4damien@gmail.com>
2020-12-01 20:43:04 +0800

    Update 1_Locks.cpp

--- 1_Locks/1_Locks/1_Locks.cpp
+++ 1_Locks/1_Locks/1_Locks.cpp
@@ -7,28 +7,59 @@
 
 using namespace std;
 
-int main()
-{
-    // *** Sec 7. Mutexes ***
-    int count = 0;  // shared data
+void work_0(int &count, mutex &mtx) {
     const int ITERATIONS = 1e6;
+
+    for (int i = 0; i < ITERATIONS; ++i) {
+        mtx.lock();     // stop more than one thread at a time
+        count++;
+        mtx.unlock();   // unstop (unlock, without do the progeam will hang)
+    }
+}
+
+void Section8_FunctionArguments() {
+    int count = 0;
     mutex mtx;
 
-    auto lambda = [&]() {
-        for (int i = 0; i < ITERATIONS; ++i) {
-            mtx.lock();     // stop more than one thread at a time
-            count++;
-            mtx.unlock();   // unstop (unlock, without do the progeam will hang)
-        }
-    };
-
-    thread t1(lambda);
-    thread t2(lambda);
+    // std::ref --- return a reference wrapper
+    thread t1(work_0, ref(count), ref(mtx));
+    thread t2(work_0, ref(count), ref(mtx));
 
     t1.join();
     t2.join();
 
     cout << count << endl;
+}
+
+void work_1(int& count, mutex& mtx) {
+    const int ITERATIONS = 1e6;
+
+    for (int i = 0; i < ITERATIONS; ++i) {
+        lock_guard<mutex> guard(mtx);    // lock mutex, unlock the mutex at the end of the loop (out of scope)
+        unique_lock<mutex> guard(mtx);  // works the same, but with more functionalities
+        count++;
+    }
+}
+
+void Section9_LockGuard() {
+    int count = 0;
+    mutex mtx;
+
+    // std::ref --- return a reference wrapper
+    thread t1(work_1, ref(count), ref(mtx));
+    thread t2(work_1, ref(count), ref(mtx));
+
+    t1.join();
+    t2.join();
+
+    cout << count << endl;
+}
+
+int main()
+{
+    //Section8_FunctionArguments();
+    Section9_LockGuard();
+
 
     return 0;
 }
